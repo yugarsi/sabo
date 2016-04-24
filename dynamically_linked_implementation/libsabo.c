690188a198d1d8daa2ac865f9cec293548152fe1
yugarsi ghosh <yugarsi@yugarsis-Air.asus1900.router>
2016-04-24 01:09:56 -0700

    fgets init commit

--- dynamically_linked_implementation/libsabo.c
+++ dynamically_linked_implementation/libsabo.c
@@ -101,73 +101,30 @@ char* gets(char *src) {
 
 //--------------------------------------------------
 
-/*
- * Read at most n-1 characters from the given file.
- * Stop when a newline has been read, or the count runs out.
- * Return first argument, or NULL if no characters were read.
- */
-char *fgets(char *buf, int n, FILE *fp)
-{   
-    size_t max = isWritable(buf);
-    if ( max == -1 ) {
+
+
+char *fgets(char *dst, int max, FILE *fp)
+{
+    size_t n = isWritable(buf);
+    if ( n == -1 ) {
         return NULL;
     }
-    if(max<n)
-        n=max;
+    if(n<max)
+        max=n;
+    int c;
+    char *p;
 
-    register size_t len;
-    register char *s;
-    register unsigned char *p, *t;
+    /* get max bytes or upto a newline */
 
-    if (n == 0)     /* sanity check */
-        return (NULL);
-
-    s = buf;
-    n--;            /* leave space for NUL */
-    while (n != 0) {
-        /*
-         * If the buffer is empty, refill it.
-         */
-        if ((len = fp->_r) <= 0) {
-            if (__srefill(fp)) {
-                /* EOF/error: stop with partial or no line */
-                if (s == buf)
-                    return (NULL);
-                break;
-            }
-            len = fp->_r;
-        }
-        p = fp->_p;
-
-        /*
-         * Scan through at most n bytes of the current buffer,
-         * looking for '\n'.  If found, copy up to and including
-         * newline, and stop.  Otherwise, copy entire chunk
-         * and loop.
-         */
-        if (len > n)
-            len = n;
-        t = memchr((void *)p, '\n', len);
-        if (t != NULL) {
-            len = ++t - p;
-            fp->_r -= len;
-            fp->_p = t;
-            (void)memcpy((void *)s, (void *)p, len);
-            s[len] = 0;
-            return (buf);
-        }
-        fp->_r -= len;
-        fp->_p += len;
-        (void)memcpy((void *)s, (void *)p, len);
-        s += len;
-        n -= len;
+    for (p = dst, max--; max > 0; max--) {
+        if ((c = fgetc (fp)) == EOF)
+            break;
+        *p++ = c;
+        if (c == '\n')
+            break;
     }
-    *s = 0;
-
-#ifdef DEBUG
-    printf("%s\n",buf);
-#endif
-    return (buf);
-
-
+    *p = 0;
+    if (p == dst || c == EOF)
+        return NULL;
+    return (p);
 }
