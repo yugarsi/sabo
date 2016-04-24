dacf616db4b8f6990ecb19a044e69c29a95b0710
sailik1991 <link2sailik@gmail.com>
2016-04-23 18:13:49 -0700

    Removing redundant code in malloc

--- dynamically_linked_implementation/libsabo.c
+++ dynamically_linked_implementation/libsabo.c
@@ -13,24 +13,19 @@ typedef struct size_aware_buffers_info {
 sabi global_array[MAX_BUFFERS];
 int count=0;
 
-
+static void* (*real_malloc)(size_t) = NULL;
 
 void *malloc(size_t size)
-{   
-    static void* (*real_malloc)(size_t) = NULL;
-    if (real_malloc == NULL) {
+{
+    if( real_malloc == NULL ) {
         real_malloc = dlsym(RTLD_NEXT, "malloc");
-        global_array[count].ptr = real_malloc;
-        global_array[count].size = size;
-        printf("Address: %p, Size: %u\n", global_array[count].ptr, global_array[count].size);
-        count ++;
-
     }
+   
     void *p = real_malloc(size);
     global_array[count].ptr = p;
     global_array[count].size = size;
-    if((int)size == 60)
-        printf("Address: %p, Size: %u\n", global_array[count].ptr, global_array[count].size);
+    printf("Address: %p, Size: %u\n", global_array[count].ptr, global_array[count].size);
+    
     count ++;
     return p;
 }
