6fba0bc462a0afcc6f1c6d5b361ad8e4e2f94a25
sailik1991 <link2sailik@gmail.com>
2016-04-23 18:43:20 -0700

    Adding isWritable utility to library

--- dynamically_linked_implementation/libsabo.c
+++ dynamically_linked_implementation/libsabo.c
@@ -4,28 +4,43 @@
 #include <dlfcn.h>
 #define MAX_BUFFERS 5000
 
+#define DEBUG
+
 typedef struct size_aware_buffers_info {
         void *ptr;
-        size_t size;
-        
+        size_t size;        
 }sabi;
 
-sabi global_array[MAX_BUFFERS];
+sabi ga[MAX_BUFFERS];
 int count=0;
 
 static void* (*real_malloc)(size_t) = NULL;
 
 void *malloc(size_t size)
 {
+    // Create override for 'malloc' symbol only at start
     if( real_malloc == NULL ) {
         real_malloc = dlsym(RTLD_NEXT, "malloc");
     }
    
+    // Create pointer, store in global table
     void *p = real_malloc(size);
-    global_array[count].ptr = p;
-    global_array[count].size = size;
-    printf("Address: %p, Size: %u\n", global_array[count].ptr, global_array[count].size);
-    
-    count ++;
+    ga[count].ptr = p;
+    ga[count].size = size;
+#ifdef DEBUG
+    printf("Address: %p, Size: %u\n", ga[count].ptr, ga[count].size);
+#endif
+    count++;
     return p;
 }
+
+int isWritable( void *p ) {
+    int i;
+    for( i=0; i<count; i++) {
+        if ( ga[i].ptr <= p && p <= ga[i].ptr + ga[i].size ) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
