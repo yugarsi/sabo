cb32957626d7d9a112546cf01e6eb09053e1c6f5
sailik1991 <link2sailik@gmail.com>
2016-04-24 18:54:42 -0700

    Adding CSE545 test_cases and status of passing the testcases (along with the commands)

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # sabo
 Size aware buffer overflow prevention library in C
 
+## Project test case status
+
+##### test1.c - PASSED
+
+```bash
+./runner.sh -f overflow_prevention/test1.c -a `python -c "print 'a'*248 +'b'*18"`
+```
+
+##### test2.c - PASSED
+
+```bash
+./runner.sh -f overflow_prevention/test2.c -a `python -c "print 'a'*248 +'b'*18"`
+```
+
+##### test3.c - PASSED
+
+```bash
+./runner.sh -f overflow_prevention/test3.c -a "`python -c "print 'a'*499 +'b'*18 + ' ' + '37_aa ' + '755'"`"
+```
+
 ## TODO
 
 #### Library framework setup for size aware buffer overflow variables: DONE
@@ -16,4 +36,5 @@ Size aware buffer overflow prevention library in C
 #### Endsuring backward compatibility
 + free: DONE
 + realloc: DONE
+
 others...
--- runner.sh
+++ runner.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 test_file="test.c"
+program_args=
 
 function usage {
-  echo "./runner.sh -f <unsafe_c_file>"
+  echo "./runner.sh -f <unsafe_c_file> -a <program_arguments>"
   echo 'default: -f test.c'
   exit
 }
 
-while getopts ":f:o" opt; do                                                                                                                    
+while getopts "f:a:" opt; do                                                                                                                    
   case $opt in                                                                                                                                     
     f)                                                                                                                                             
       test_file=$OPTARG
+      ;;
+    a)
+      program_args=$OPTARG
       ;;
     \?)                                                                                                                                            
       usage                                                                                                                                        
@@ -19,12 +23,13 @@ while getopts ":f:o" opt; do
   esac                                                                                                                                             
 done
 
+echo "$program_args"
+
 # Create output for test file
-gcc -o ${test_file}.o $test_file 
-chmod +x ${test_file}.o
+gcc $test_file
 
 # Create library file for dynamic linking
 gcc -Wall  -shared -fPIC -o libsabo.so libsabo.c -ldl
 
 # Run test code with libsabo library
-LD_PRELOAD=$PWD/libsabo.so ./${test_file}.o
+LD_PRELOAD=$PWD/libsabo.so ./a.out $program_args
